d61c8bb09e3f5b80be8a218a7b0cedca8419bb6a
alskqp501 <alskqp501@gmail.com>
2022-05-16 19:41:38 +0900

    board initial setting

---
+++ src/main/java/com/example/board/domain/entity/TimeEntity.java
@@ -0,0 +1,4 @@
+package com.example.board.domain.entity;
+
+public class TimeEntity {
+}
--- src/test/java/com/example/board/BoardApplicationTests.java
+++ src/test/java/com/example/board/BoardApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.board;
 
+import com.example.board.Controller.BoardController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class BoardApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
